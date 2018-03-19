cb9c62ba2892b074149a1ccd5424c5d19b8218f2
Mcarrions <mcarrions@uees.edu.ec>
2018-03-19 16:10:52 -0500

    Correccion casi lista

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 var usuarios = [{
     "email": "alejandro@hotmail.com",
-    "password": "alejandro"
-}, {
-    "email": "prueba",
-    "password": "prueba"
+    "password": "alejandro",
+	balanceUsuario: 0
 }]
+
+balance = usuarios[0].balanceUsuario;
 
 $(document).ready(function () {
 
@@ -32,3 +32,73 @@ $(document).ready(function () {
     });
 
 });
+
+function calcularIngreso() {
+	var valor = document.getElementById("valorIn").value;
+	if(isNaN(valor) || parseFloat(valor)==0){
+		alert("Ingresar un numero o valor correcto!");
+	}
+	else{
+		balance = balance + parseFloat(valor);
+		document.getElementById("balanceDisplay").innerHTML = balance;
+		usuarios[0].balanceUsuario = balance;
+	}
+    
+}
+
+function calcularEgreso() {
+	var valor = document.getElementById("valorEg").value;
+	if(isNaN(valor) || parseFloat(valor)==0.0){
+		alert("Ingresar un numero o valor correcto!");
+	}
+	else{
+		balance = balance - parseFloat(valor);
+		document.getElementById("balanceDisplay").innerHTML = balance;
+		usuarios[0].balanceUsuario = balance;
+	}
+}
+
+function getBalance(){
+	document.getElementById("balanceDisplay").innerHTML = balance;
+}
+
+function setUserBalanceCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function checkCookie(){
+	var userBalance=getCookie("userBalance");
+     if (user != ""){
+		 this.balance = parseFloat(user);
+	 }
+	 else if(user != "0"){
+		this.balance = parseFloat(user);
+	 }
+	 else{
+		  setCookie("userBalance", 0, 30);
+		  userBalance=getCookie("userBalance");
+		  this.balance = parseFloat(user);
+	 }
+	
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
